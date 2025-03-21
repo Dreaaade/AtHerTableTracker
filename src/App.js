356b375065f62ed6a56f4a2218bc456fb8e36f95
Dreaaade <dreaaade@gmail.com>
2025-03-21 14:13:09 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PotluckTracker from "./PotluckTracker"; // Import the PotluckTracker component
+
+function App() {
+  return (
+    <div>
+      <PotluckTracker /> {/* Use the PotluckTracker component here */}
+    </div>
+  );
+}
+
+export default App;
